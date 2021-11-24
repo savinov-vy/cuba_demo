5bc7c33bb3c9390833c061675914c8c3af4afcb2
savinov <savinov_vu@mail.ru>
2021-11-24 18:03:49 +0800

    add possibility edit session on click mouse on calendar

--- modules/web/src/com/company/planner/web/screens/session/SessionBrowse.java
+++ modules/web/src/com/company/planner/web/screens/session/SessionBrowse.java
@@ -1,11 +1,25 @@
 package com.company.planner.web.screens.session;
 
+import com.haulmont.cuba.gui.components.Calendar;
+import com.haulmont.cuba.gui.components.EditorScreenFacet;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.planner.entity.Session;
+
+import javax.inject.Inject;
+import java.time.LocalDateTime;
 
 @UiController("planner_Session.browse")
 @UiDescriptor("session-browse.xml")
 @LookupComponent("sessionsTable")
 @LoadDataBeforeShow
 public class SessionBrowse extends StandardLookup<Session> {
+    @Inject
+    private EditorScreenFacet<Session, SessionEdit> sessionEditDialog;
+
+    @Subscribe("sessionsCalendar")
+    public void onSessionsCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<LocalDateTime> event) {
+        sessionEditDialog.setEntityProvider(() -> (Session) event.getEntity());
+        sessionEditDialog.show();
+    }
+
 }
